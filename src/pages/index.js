c72f37511cc4efa022e18fa60d442706d420b13b
kentsin <kentsin@gmail.com>
2021-02-18 11:39:28 +0800

    excel-transform-test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 style={{ color: `purple`, fontSize: `72px` }}>Hi people</h1>
     <p>Welcome to my new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
